cb6515ddf3e9a9486226eafcf8de6292fbab98ec
Pau Julian <pau.julian@gmail.com>
2025-02-27 09:52:28 +0100

    fix/ add StatusBar in app entry point

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,14 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../graphql/apolloClient";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 
 export default function Layout() {
     return (
         <ApolloProvider client={client}>
             <SafeAreaProvider>
+                <StatusBar style='auto' />
                 <Stack
                     screenOptions={{
                         headerShown: false,
